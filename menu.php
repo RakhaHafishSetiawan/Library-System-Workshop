79bacac6d67bd0b26bd8d21893ca0633c006e879
Rakha Hafish Setiawan <102712597+RakhaHafishSetiawan@users.noreply.github.com>
2022-04-09 09:46:55 +0700

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,12 @@
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+	<div class="navbar-header">
+	  <a class="navbar-brand" href="index.php">Perpustakaan Kita</a>
+	</div>
+	<ul class="nav navbar-nav">
+	  <li><a href="data-nama.php">Data Nama</a></li>
+	  <li><a href="#">Menu 2</a></li>
+	  <li><a href="#">Menu 3</a></li>
+	</ul>
+  </div>
+</nav>
