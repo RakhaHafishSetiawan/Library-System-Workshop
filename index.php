179e2cd5f72a8dd2c298e9b7f068f9995ee4400d
Rakha Hafish Setiawan <102712597+RakhaHafishSetiawan@users.noreply.github.com>
2022-04-09 09:35:13 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>PHP CRUD</title>
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="container">
+	<?php include "menu.php"; ?>
+    <h1>Selamat Datang!</h1>
+	</div>
+</body>
+</html>
